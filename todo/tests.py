6deae070fc765f14c9d57bbf2654ff1554054a0f
1F10250095 <s1f102500955@iniad.org>
2026-07-09 14:11:20 +0900

    Update tests.py

--- todo/tests.py
+++ todo/tests.py
@@ -117,8 +117,9 @@ class TodoViewTestCase(TestCase):
 
     def test_delete_success(self):
         task = Task(
-        title='task1',
-        due_at=timezone.make_aware(datetime(2024, 7, 1)))
+            title='task1',
+            due_at=timezone.make_aware(datetime(2024, 7, 1))
+        )
         task.save()
         client = Client()
         response = client.get('/{}/delete'.format(task.pk))
